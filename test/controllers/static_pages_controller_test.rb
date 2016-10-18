554e81f6fb3e46b27f3f24bac9e53f28ff2381f1
Karena Stevens <kaymark09@gmail.com>
2016-10-18 13:10:37 -0700

    Finish static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Karena Stevens"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Karena Stevens"
+  end
+
+  test "should get portfolio" do
+    get static_pages_portfolio_url
+    assert_response :success
+    assert_select "title", "Portfolio | Karena Stevens"
+  end
+
+  test "should get blog" do
+    get static_pages_blog_url
+    assert_response :success
+    assert_select "title", "Blog | Karena Stevens"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Karena Stevens"
+  end
+
+end
