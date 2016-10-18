d3323a757ab0499dd23af3175e1abe61a515c4d8
Karena Stevens <kaymark09@gmail.com>
2016-10-18 13:57:07 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/portfolio'
-  get 'static_pages/blog'
-  get 'static_pages/contact'
+  get '/about',     to: 'static_pages#about'
+  get '/portfolio', to: 'static_pages#portfolio'
+  get '/blog',      to: 'static_pages#blog'
+  get '/contact',   to: 'static_pages#contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,31 +2,31 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Karena Stevens"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Karena Stevens"
   end
 
   test "should get portfolio" do
-    get static_pages_portfolio_url
+    get portfolio_path
     assert_response :success
     assert_select "title", "Portfolio | Karena Stevens"
   end
 
   test "should get blog" do
-    get static_pages_blog_url
+    get blog_path
     assert_response :success
     assert_select "title", "Blog | Karena Stevens"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Karena Stevens"
   end
