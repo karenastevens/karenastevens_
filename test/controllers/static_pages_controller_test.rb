e084703cc2df156f1ebd72ecc96471f71bf2caf2
Karena Stevens <kaymark09@gmail.com>
2016-10-18 13:20:23 -0700

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Karena Stevens"
+    assert_select "title", "Karena Stevens"
   end
 
   test "should get about" do
