44d381eb59de44252d5007ed48956aa27e6feb21
Karena Stevens <kaymark09@gmail.com>
2016-10-24 16:49:57 -0700

    Finish contact form with testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'static_pages#home'
   get    '/about',      to: 'static_pages#about'
   get    '/portfolio',  to: 'static_pages#portfolio'
   get    '/blog',       to: 'posts#index'
-  get    '/contact',    to: 'static_pages#contact'
+  get    '/contact',    to: 'messages#new'
+  post   '/contact',    to: 'messages#create'
   get    '/login',      to: 'sessions#new'
   post   '/login',      to: 'sessions#create'
   delete '/logout',     to: 'sessions#destroy'
   resources :users
   resources :posts
+  resources :messages, only: [:new, :create]
 end
