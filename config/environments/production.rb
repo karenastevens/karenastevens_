3629c0bb192c4c5998b73811c498b4b5a105c837
Karena Stevens <kaymark09@gmail.com>
2016-10-25 11:41:24 -0700

    Added line to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
