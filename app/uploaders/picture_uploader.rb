bbf3b444822cc035b0378e190918c50f67c44d50
Karena Stevens <kaymark09@gmail.com>
2016-10-24 23:12:39 -0700

    Change Image Magick resize limit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,7 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [200, 200]
+  process resize_to_limit: [400, 400]
 
   storage :file
 
