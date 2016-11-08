a9480c41ed71cb8f67a2839e088e583571728a77
Karena Stevens <kaymark09@gmail.com>
2016-11-07 20:15:02 -0800

    Added markdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+Post.create(
+  user_id: 1,
+  title: "Markdown Post",
+  created_at: Time.now,
+  content: 
+    %Q{### There Is Something You Should Know!
+ 
+  This is my very first post using markdown!
+ 
+  How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!}
+  )
