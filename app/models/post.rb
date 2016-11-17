3b5b48a53f93a539d47130ee3e98a3c8303a5cbe
Karena Stevens <kaymark09@gmail.com>
2016-11-16 20:43:46 -0800

    Change blog format

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
-  validate  :picture_size
 
   def feed
     Post.all
   end
-
-  private
-
-    # Validates the size of an uploaded picture.
-    def picture_size
-      if picture.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
-      end
-    end
 end
+  
